212cbc112925704291573c694b714c50720ddf4b
faipena <196348511+faipena@users.noreply.github.com>
2025-01-24 21:11:54 +0100

    progress

--- src/twitch/api.ts
+++ src/twitch/api.ts
@@ -1,12 +1,36 @@
+import TwitchCredentials from "./types.ts";
+
 const API_ID_URL = "https://id.twitch.tv";
 const API_HELIX_URL = "";
 
 export default class TwitchAPI {
     readonly id;
     readonly secret;
+    credentials?: TwitchCredentials;
+    // token: string;
 
     constructor(id: string, secret: string) {
         this.id = id;
         this.secret = secret;
+    }
+
+    // deno-lint-ignore no-explicit-any
+    private async http(url: string, params?:object): Promise<any> {
+        const httpMethod = params ? "POST" : "GET"
+        const response = await fetch(`${url}`, {
+            method: httpMethod,
+            headers: params ? new Headers({"content-type": "application/json"}) : undefined,
+            body: params ? JSON.stringify(params) : undefined,
+        });
+        if(!response.ok) throw Error(`Invalid response from twitch server (HTTP ${response.status}): ${await response.text()}`)
+        return await response.json();
+    }
+
+    async getToken() {
+        this.credentials = await this.http(`${API_ID_URL}/oauth2/token`, {
+            client_id: this.id,
+            client_secret: this.secret,
+            grant_type: "client_credentials",
+        });
     }
 }
--- src/twitch/twitch.ts
+++ src/twitch/twitch.ts
@@ -1,18 +1,18 @@
+import TwitchAPI from "./api.ts";
+
 const TWITCH_URL = "http://twitch.tv"
 export default class Twitch {
     readonly channel: string;
     readonly url : string;
     readonly db: Deno.Kv;
-    readonly clientId: string;
-    readonly clientSecret: string;
+    api : TwitchAPI;
     timer?: number;
 
     constructor(channel: string, db: Deno.Kv, clientId: string, clientSecret: string) {
         this.channel = channel;
         this.url = `${TWITCH_URL}/${channel}`;
         this.db = db;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
+        this.api = new TwitchAPI(clientId, clientSecret);
     }
 
     private getToken() {
@@ -25,22 +25,7 @@ export default class Twitch {
 
     startMonitoring(interval: number = 5000) {
         this.timer = setInterval(async () => {
-            try {
-                const response = await fetch(this.url);
-                if (!response.ok) return;
-                const isLive = (await response.text()).includes('isLiveBroadcast":true');
-                const wasLive : boolean = (await this.db.get(["twitch", this.channel])).value === true;
-                if (wasLive && !isLive) {
-                    await this.db.set(["twitch", "streamStatus", this.channel], false);
-                    if (this.onOffline) this.onOffline();
-                } else if (isLive && !wasLive) {
-                    await this.db.set(["twitch", "streamStatus", this.channel], true);
-                    if (this.onOnline) this.onOnline();
-                }
-            }
-            catch (error) {
-                console.warn(`Twitch.monitor(): ${error}`)
-            }
+            await this.api.getToken();
         }, interval);
     }
 
